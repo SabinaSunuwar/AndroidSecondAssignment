492f820672125ac50a7e3d173944f79f9275b43e
Sabina Sunuwar <sabinasunuwar4@gmail.com>
2019-12-10 15:45:59 +0545

    Source Code for Area of Circle Added.

--- app/src/main/java/com/softwarica/androidsecondassignment/Fragments/AreaofCircle.java
+++ app/src/main/java/com/softwarica/androidsecondassignment/Fragments/AreaofCircle.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.softwarica.androidsecondassignment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AreaofCircle extends Fragment {
+public class AreaofCircle extends Fragment implements View.OnClickListener {
+
+    private Button btnAreaofCircle;
+    private EditText etRadius;
 
 
     public AreaofCircle() {
@@ -26,7 +32,22 @@ public class AreaofCircle extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_areaof_circle, container, false);
+        View view = inflater.inflate(R.layout.fragment_areaof_circle, container, false);
+
+        etRadius = view.findViewById(R.id.etRadius);
+        btnAreaofCircle = view.findViewById(R.id.btnAreaofCircle);
+
+        btnAreaofCircle.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        float radius = Integer.parseInt(etRadius.getText().toString());
+        float area = 3.14f * radius * radius;
+
+        Toast.makeText(getActivity(),"Area of Circle" + area, Toast.LENGTH_SHORT).show();
+
+    }
 }
--- app/src/main/java/com/softwarica/androidsecondassignment/MainActivity.java
+++ app/src/main/java/com/softwarica/androidsecondassignment/MainActivity.java
@@ -1,14 +1,57 @@
 package com.softwarica.androidsecondassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.softwarica.androidsecondassignment.Fragments.AreaofCircle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAreaofCircle, btnPalindrome, btnSimpleInterest, btnArmstrong,
+            btnAutomorphic, btnSwapping;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAreaofCircle = findViewById(R.id.btnAreaofCircle);
+        btnPalindrome = findViewById(R.id.btnPalindrome);
+        btnSimpleInterest = findViewById(R.id.btnSimpleInterest);
+        btnArmstrong = findViewById(R.id.btnArmstrong);
+        btnAutomorphic = findViewById(R.id.btnAutomorphic);
+        btnSwapping = findViewById(R.id.btnSwapping);
+
+        btnAreaofCircle.setOnClickListener(this);
+        btnPalindrome.setOnClickListener(this);
+        btnSimpleInterest.setOnClickListener(this);
+        btnArmstrong.setOnClickListener(this);
+        btnAutomorphic.setOnClickListener(this);
+        btnSwapping.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (v.getId()) {
+            case R.id.btnAreaofCircle:
+
+                AreaofCircle areaofCircle = new AreaofCircle();
+                fragmentTransaction.replace(R.id.fragmentContainer,areaofCircle);
+                fragmentTransaction.commit();
+                break;
+        }
+
+
     }
 }
